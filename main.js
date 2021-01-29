d735b7977efd9638352e6253391eb0ed5d7296fe
Ben Hamelin <bhamelin@oomphinc.com>
2021-01-28 19:24:44 -0500

    Initial commit

---
+++ main.js
@@ -0,0 +1,11 @@
+const access_code = "bcmlxh";
+const vowel_amount=100;
+const num_players=2;
+
+function spin() {
+
+    var x = Math.ceil (Math.random() *10) *100;
+    document.getElementById('wheel').value=x
+
+
+}
